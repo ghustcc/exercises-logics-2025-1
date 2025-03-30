e4f62547b6f555c0707e00b0eb09442d5f5af2cb
Gustavo Cardoso Costa <gustavocardoso0019@gmail.com>
2025-03-30 16:00:37 -0300

    up

---
+++ C/exe13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int A,B,C;
+
+    printf("Digite o valor de A: ");
+    scanf("%d", &A);
+    printf("Digite o valor de B: ");
+    scanf("%d", &B);
+    printf("Digite o valor de C: ");
+    scanf("%d", &C);
+    
+    A > B  && B > C ? printf("%i -> %i -> %i", A,B,C) : A > B &&    ASDF;
+
+    return 0;
+}
--- IP/exercicios_1/exe3.c
+++ IP/exercicios_1/exe3.c
@@ -3,20 +3,15 @@
 
 int main()
 {
-    double a,b,c,d,e,x;
+    float a,b,c,d,e;
 
-    printf("Number a: ");
-    scanf("%f", &a);
-    printf("Number b: ");
-    scanf("%f", &b);
-    printf("Number c: ");
-    scanf("%f", &c);
-    printf("Number d: ");
-    scanf("%f", &d);
-    printf("Number e: ");
-    scanf("%f", &e);
+    printf("Number a: "); scanf("%f", &a);
+    printf("Number b: "); scanf("%f", &b);
+    printf("Number c: "); scanf("%f", &c);
+    printf("Number d: "); scanf("%f", &d);
+    printf("Number e: "); scanf("%f", &e);
 
-    x = (pow(a, 3)) * ((b+c)/d + e);
+    float x = (pow(a, 3)) * ((b+c)/d + e);
 
     printf("x = %.2f", x);
     return 0;
