1201999212a9ded8e799237fe017b1885028c51a
Gustavo Cardoso Costa <135393167+ghustcc@users.noreply.github.com>
2025-04-16 14:52:10 -0300

    Update chess.c

--- C/chess.c
+++ C/chess.c
@@ -2,19 +2,24 @@
 
 int main()
 {
-    char positions[8] = {'a','b','c','d','e','f','g','h'};
+    char positions[8] = {'A','B','C','D','E','F','G','H'};
     int x, y;
 
     printf("Position x (1 at 8): "); scanf("%i", &x);
     printf("Position y (1 at 8): "); scanf("%i", &y);
 
-    printf("\n ");
+    printf("\n   ");
     for (int pos = 0; pos < 8; pos++) {
         printf(" %c ", positions[pos]);
     }
+    
+    printf("\n   ");
+    for (int pos = 0; pos < 10; pos++) {
+        printf("──");
+    }
 
     for (int i = 0; i < 8; i++) {
-        printf("\n%i", (i+1));
+        printf("\n%i │", (i+1));
         for (int j = 0; j < 8; j++) {
             if ((j+1) == x || (i+1) == y) {
                 printf(" o ");
@@ -23,6 +28,17 @@ int main()
                 printf(" # ");
             }
         }
+        printf("| %i", (i+1));
+    }
+    
+    printf("\n   ");
+    for (int pos = 0; pos < 10; pos++) {
+        printf("──");
+    }
+    
+    printf("\n   ");
+    for (int pos = 0; pos < 8; pos++) {
+        printf(" %c ", positions[pos]);
     }
 
     return 0;
