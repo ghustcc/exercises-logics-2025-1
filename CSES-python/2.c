dd356361f51ff2c6b11874140fc78708a774c1c0
Gustavo Cardoso Costa <gustavocardoso0019@gmail.com>
2025-04-22 21:45:20 -0300

    update exercises

--- CSES-python/2.c
+++ CSES-python/2.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() 
+int main()
 {
     int n;
+    int x;
     scanf("%d", &n);
-    
-    int numbers[n-1];
-    if (n >= 2 && n <= 2*10*10*10*10*10) 
-    {
-        for (int i = 0; i <= n-2; i++) {
-            scanf("%d", &numbers[i]);
-        }
 
-        for (int i = 0; i <= n-2; i++) {
-            if (numbers[i] != (i+1)) {
-                printf("%d\n", (i+1));
-                break;
-            }
-        } 
+    int numbers[n-1];
+
+    for (int i = 0; i <= n-2; i++) {
+        scanf("%d", &x);
+        numbers[i] = x;
     }
+
+    for (int i = 0; i <= n-2; i++) {
+        if ((i+1) != numbers[i]) {
+            printf("%d\n", (i+1));
+            break;
+        }
+    } 
 
     return 0;
 }
