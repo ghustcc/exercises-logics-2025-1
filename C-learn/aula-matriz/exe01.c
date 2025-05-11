5834fc78233012266eec172b9ccd73d1570a7b53
Gustavo Cardoso Costa <gustavocardoso0019@gmail.com>
2025-05-11 20:05:44 -0300

    feat: revision ponteiros e strings

--- C-learn/aula-matriz/exe01.c
+++ C-learn/aula-matriz/exe01.c
@@ -4,21 +4,30 @@
 int main () {
 
     int i, j;
-    char palavras[3][20];
+    char palavras[3][21]; // 20 + 1 for the null terminator
+    // char *palavras[3]; // array de 3 ponteiros
 
     for (i = 0; i < 3; i++) {
-        fgets(palavras[i], 20, stdin);
+        scanf("%s", palavras[i]);
+        palavras[i][21] = '\0'; // Ensure null termination
+        // fgets(palavras[i], 21, stdin);
+        // palavras[i][strcspn(palavras[i], "\n")] = '\0'; // Remove newline character
     }
 
-    printf("%d", strlen(palavras[0]));
-    printf("%d", strlen(palavras[1]));
-    printf("%d", strlen(palavras[2]));
+
+    printf("%d,", strlen(palavras[0]));
+    printf("%d,", strlen(palavras[1]));
+    printf("%d\n", strlen(palavras[2]));
+
+    printf("%s,", palavras[0]);
+    printf("%s,", palavras[1]);
+    printf("%s\n", palavras[2]);
 
     for (i = 0; i < 3; i++) {
         for (j = 0; j < strlen(palavras[i]) / 2; j++) {
             char temp = palavras[i][j];
-            palavras[i][j] = palavras[i][strlen(palavras[i]) - j - 2];
-            palavras[i][strlen(palavras[i]) - j - 2] = temp;
+            palavras[i][j] = palavras[i][strlen(palavras[i]) - j - 1];
+            palavras[i][strlen(palavras[i]) - j - 1] = temp;
         } 
     }
 
