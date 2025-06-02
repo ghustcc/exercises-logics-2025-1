d4c314daf27a159f993eb6bc718a345c587ba48c
Gustavo Cardoso Costa <gustavocardoso0019@gmail.com>
2025-06-01 23:35:23 -0300

    feat: conclude list BOCA_6

--- IP-lists/BOCA_6/f10.c
+++ IP-lists/BOCA_6/f10.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    int N, M, i, j, limiteN;
+    int N, M, i, j, limiteN, limiteM, count = 0;
     
     scanf("%d %d", &N, &M);
     
@@ -12,19 +12,30 @@ int main() {
         for (j = 0; j < M; j++)
             scanf("%d", &matriz[i][j]);
     }
+
     limiteN = N;
+    limiteM = M;
     for (i = 0; i < limiteN; i++) {
-        for (j = i; j < limiteN; j++) {
-            if (i == 0) {
-                printf("%d ", matriz[i][j]);
-                if (j == limiteN - 1 && j > 0) {
-                    printf("%d ", matriz[j][limiteN - 1]);
-                    if (i == 0 && j == limiteN - 1 && j > 0)
-                        printf("%d ", matriz[j][limiteN - 1]);
-                }
-            }
+        for (j = i; j < limiteM; j++) {
+            printf("%d ", matriz[i][j]);
+            count++;
         }
-        limiteN -= 1;
+        for (j = i; j < limiteN && count < M*N; j++) {
+            if (j > i) {
+                printf("%d ", matriz[j][limiteM - 1]);
+                count++;
+            }        
+        }
+        for (j = limiteM - 2; j >= i && count < M*N; j--) {
+            printf("%d ", matriz[limiteN - 1][j]);    
+            count++;
+        }
+        for (j = limiteN - 2; j > i && count < M*N; j--) {
+            printf("%d ", matriz[j][i]);
+            count++;
+        }
+        limiteN--;
+        limiteM--;
     }
 
     return 0;
--- IP-lists/BOCA_6/f8.c
+++ IP-lists/BOCA_6/f8.c
@@ -1,36 +1,53 @@
 #include <stdio.h>
+#include <math.h>
 
-#define E 2.71828
+#define E 2.7182818
+int N, M;
+double softmax(int line, int x, int matriz[N][M]);
 
 int main() {
 
-    int N, M, i, j, maior;
+    int i, j;
     
     scanf("%d %d", &N, &M);
-    
+
     int matriz[N][M];
-    int vet_maior_linha[N];
 
     for (i = 0; i < N; i++) {
         for (j = 0; j < M; j++)
             scanf("%d", &matriz[i][j]);
     }
-    for (i = 0; i < N; i++) {
-        maior = 0;
-        for (j = 0; j < M; j++) {
-            if (matriz[i][j] > maior)
-                maior = matriz[i][j];}
-        vet_maior_linha[i] = maior;
-    }
 
     for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++)
-            printf("%.6f ", (matriz[i][j] / (float) vet_maior_linha[i]));
+        for (int j = 0; j < M; j++) {
+            printf("%.6lf ", softmax(i, matriz[i][j], matriz));
+        }
     }
 
     return 0;
 }
 
-float softmax() {
-    
+double softmax(int line, int x, int matriz[N][M]) {
+    long double somatorio = 0.0, somatorio1 = 0.0;
+    for (int j = 0; j < M; j++) {
+        if (matriz[line][j] % 2 == 0) {
+            if (matriz[line][j] > 500)
+                somatorio1 += pow(E, matriz[line][j] / 2) * pow(E, matriz[line][j] / 2);
+            else
+                somatorio += pow(E, matriz[line][j]);
+        }
+        else {
+            if (matriz[line][j] > 500)
+                somatorio1 += pow(E, matriz[line][j] / 2) * pow(E, matriz[line][j] / 2 + 1);
+            else
+                somatorio += pow(E, matriz[line][j]);
+        }
+    }
+    if (x % 2 == 0 && x > 500)
+        return pow(E, x / 2) * pow(E, x / 2) / somatorio1;
+    if (x % 2 != 0 && x > 500)
+        return ((pow(E, x / 2) * pow(E, x / 2 + 1)) / somatorio1);
+    if (x < 500)
+        return pow(E, x) / somatorio;
+    return 0.0;
 }
